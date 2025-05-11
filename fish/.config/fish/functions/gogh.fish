8c91388f8fddf5e5de7a4e9623b0ba6a564d6515
CJ Sabijon <cjsabijon@gmail.com>
2025-05-11 21:51:27 +0800

    updated alias

---
+++ fish/.config/fish/functions/gogh.fish
@@ -0,0 +1,4 @@
+function gogh --wraps='bash -c  "$(wget -qO- https://git.io/vQgMr)"' --description 'alias gogh=bash -c  "$(wget -qO- https://git.io/vQgMr)"'
+  bash -c  "$(wget -qO- https://git.io/vQgMr)" $argv
+        
+end
