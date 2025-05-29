1efc98c2b276e7b91eb7fd107fe465f987b6e44b
CJ Sabijon <cjsabijon@gmail.com>
2025-05-29 16:43:08 +0800

    updated config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,6 +7,19 @@ oh-my-posh init fish --config $HOME/dotfiles/ohmyposh/.config/ohmyposh/catppucci
 #Zoxide
 zoxide init --cmd cd fish | source
 
+#Fz
+fzf --fish | source
+
+#Yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
